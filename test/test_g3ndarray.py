0b7138aa82be1e2e9d3d7be119ed21d596d60051
Matthew Hasselfield <mhasself@users.noreply.github.com>
2019-02-07 18:33:08 -0500

    Update test_g3ndarray.py

--- test/test_g3ndarray.py
+++ test/test_g3ndarray.py
@@ -15,9 +15,16 @@ w = core.G3Writer('out.g3')
 f = core.G3Frame()
 f['x'] = x
 w.Process(f)
+
+# And the autoconverter?
+f = core.G3Frame()
+f['y'] = 1
+f['x'] = np.arange(1000.)
+w.Process(f)
+
 del w
 
-# read that back?
+# read those back?
 
 print('Readback:')
 for f in core.G3File('out.g3'):
