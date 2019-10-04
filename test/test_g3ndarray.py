09bf5356b2aa6fe82cbbdfb57c348a73960f59d9
Brian Koopman <brian.koopman@yale.edu>
2019-10-04 14:49:12 -0400

    Skip enmap test if pixell not found

--- test/test_g3ndarray.py
+++ test/test_g3ndarray.py
@@ -2,8 +2,16 @@ import so3g
 core = so3g.spt3g_core
 
 import numpy as np
-
 import gc
+import unittest
+
+# Don't require pixell for testing
+try:
+    from pixell import enmap
+    pixell_found = True
+except ModuleNotFoundError:
+    pixell_found = False
+
 print('Creating y')
 y = np.arange(100,200000, dtype=float)
 x = so3g.G3Ndarray(y)
@@ -29,8 +37,12 @@ print('Readback:')
 for f in core.G3File('out.g3'):
     print(f['x'])
 
-# enmap?
-from pixell import enmap
-m = enmap.zeros((100,20))
-f = core.G3Frame()
-f['test_map'] = m
+class TestG3NDArray(unittest.TestCase):
+    """TestCase for testing G3NDArrays."""
+    @unittest.skipIf(pixell_found is False, "pixell not found")
+    def test_enmap(self):
+        """Test writing enmap objects to G3Frames."""
+        # enmap?
+        m = enmap.zeros((100,20))
+        f = core.G3Frame()
+        f['test_map'] = m
