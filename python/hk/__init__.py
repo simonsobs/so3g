cf64d615083402f59fa4786d0ee3058628be6618
Katie Harrington <kmharrin@umich.edu>
2020-03-12 13:39:09 -0400

    add load_range to init file

--- python/hk/__init__.py
+++ python/hk/__init__.py
@@ -1,7 +1,7 @@
 """
 Support for the SO Housekeeping format.
 """
-from .getdata import HKArchive, HKArchiveScanner
+from .getdata import HKArchive, HKArchiveScanner, load_range
 from .reframer import HKReframer
 from .scanner import HKScanner
 from .session import HKSessionHelper
