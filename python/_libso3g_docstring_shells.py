770b4a828e465269580fc811d5dff85d63bb2a48
Matthew Hasselfield <mhasself@users.noreply.github.com>
2020-04-17 10:28:37 -0400

    Doc-ready build : 0.0.7+34.gd955704.dirty

--- python/_libso3g_docstring_shells.py
+++ python/_libso3g_docstring_shells.py
@@ -23,10 +23,14 @@
 
 # No handler for "os"
 
-# No handler for "_libso3g_docstring_shells"
+# No handler for "spt3g_core"
+
+# No handler for "load_pybindings"
 
 def version():
-  return "0.0.7+15.g1da0837"
+  return "0.0.7+34.gd955704.dirty"
+# No handler for "greet"
+
 class TestClass:
   """None"""
   pass
@@ -716,6 +720,194 @@ class BFilterBank:
 
     C++ signature :
         void apply(BFilterBank {lvalue},boost::python::api::object,boost::python::api::object)"""
+    pass
+
+class RangesInt32:
+  """A finite series of non-overlapping semi-open intervals on a domain
+of type: int32_t.
+
+To create an empty object, instantiate with just a sample count:
+``RangesInt32(count)``.
+
+Alternately, consider convenience methods such as ``from_mask``,
+``from_array``, and ``from_bitmask``; see below.
+
+In addition to the methods explained below, note the that following
+operators have been defined and perform as follows (where ``r1`` and
+``r2`` are objects of this class:
+
+- ``~r1`` is equivalent to ``r1.complement()``
+- ``r1 *= r2`` is equivalent to ``r1.intersect(r2)``
+- ``r1 += r2`` is equivalent to ``r1.merge(r2)``
+- ``r1 * r2`` and ``r1 + r2`` behave as you might expect, returning a
+  new object and leaving ``r1`` and ``r2`` unmodified.
+
+The object also supports slicing.  For example, if ``r1`` has
+count = 100 then r1[10:-5] returns a new object (not a view)
+that has count = 85.  A data member ``reference`` keeps track
+of the history of shifts in the first sample; for example if
+r1.reference = 0 then r1[10:-5].reference will be -10.  This
+variable can be interpreted as giving the logical index, in
+the new index system, of where index=0 of the original object
+would be found.  This is useful for bookkeeping in some cases.
+"""
+  pass
+  def add_interval():
+    """add_interval(start, end) -> RangesInt32
+    Merge an interval into the set."""
+    pass
+  def append_interval_no_check():
+    """append_interval_no_check(start, end) -> RangesInt32
+    Append an interval to the set without checking for overlap or sequence."""
+    pass
+  def merge():
+    """merge(src) -> RangesInt32
+    Merge ranges from another RangesInt32 into this one."""
+    pass
+  def intersect():
+    """intersect(src) -> RangesInt32
+    Intersect another RangesInt32 with this one."""
+    pass
+  def complement():
+    """complement() -> RangesInt32
+    Return the complement (over domain)."""
+    pass
+  def ranges():
+    """ranges() -> object
+    Return the intervals as a 2-d numpy array of ranges."""
+    pass
+  @staticmethod
+  def from_array():
+    """from_array(data, count) -> RangesInt32
+    The input data must be an (n,2) shape ndarray of int32. The integer count sets the domain of the object."""
+    pass
+  @staticmethod
+  def from_bitmask():
+    """from_bitmask(bitmask_array) -> object
+    Return a list of RangesInt32 extracted from an ndarray encoding a bitmask."""
+    pass
+  @staticmethod
+  def from_mask():
+    """from_mask(bool_array) -> object
+    Return a list of RangesInt32 extracted from an ndarray of bool."""
+    pass
+  @staticmethod
+  def bitmask():
+    """bitmask(ranges_list, n_bits) -> object
+    Return an ndarray bitmask from a list ofRangesInt32.
+    n_bits determines the output integer type.  Bits are assigned from 
+    LSB onwards; use None in the list to skip a bit."""
+    pass
+  def mask():
+    """mask() -> object
+    Return a boolean mask from this Ranges object."""
+    pass
+  def copy():
+    """copy() -> RangesInt32
+    Get a new object with a copy of the data."""
+    pass
+
+class std_map_indexing_suite__MapRangesInt32BaseMap_entry:
+  """None"""
+  pass
+  def data():
+    """data() -> RangesInt32
+    K.data() -> the value associated with this pair.
+    """
+    pass
+  def key():
+    """key() -> str
+    K.key() -> the key associated with this pair.
+    """
+    pass
+  def first():
+    """first() -> str
+    K.first() -> the first item in this pair.
+    """
+    pass
+  def second():
+    """second() -> RangesInt32
+    K.second() -> the second item in this pair.
+    """
+    pass
+
+class MapRangesInt32:
+  """Mapping from strings to Ranges over int32_t."""
+  pass
+  def keys():
+    """keys() -> list
+    D.keys() -> list of D's keys
+    """
+    pass
+  def has_key():
+    """has_key(arg2) -> bool
+    D.has_key(k) -> True if D has a key k, else False
+    """
+    pass
+  def values():
+    """values() -> list
+    D.values() -> list of D's values
+    """
+    pass
+  def items():
+    """items() -> list
+    D.items() -> list of D's (key, value) pairs, as 2-tuples
+    """
+    pass
+  def clear():
+    """clear() -> None
+    D.clear() -> None.  Remove all items from D.
+    """
+    pass
+  def copy():
+    """copy() -> MapRangesInt32
+    D.copy() -> a shallow copy of D
+    """
+    pass
+  def get():
+    """get(arg2, default_val) -> object
+    D.get(k[,d]) -> D[k] if k in D, else d.  d defaults to None.
+    """
+    pass
+  def pop():
+    """pop( (MapRangesInt32)arg1, (str)arg2) -> object
+
+pop( (MapRangesInt32)arg1, (str)arg2, (object)arg3) -> object :
+    D.pop(k[,d]) -> v, remove specified key and return the corresponding value
+    If key is not found, d is returned if given, otherwise KeyError is raised
+    """
+    pass
+  def popitem():
+    """popitem() -> object
+    D.popitem() -> (k, v), remove and return some (key, value) pair as a
+    2-tuple; but raise KeyError if D is empty
+    """
+    pass
+  @staticmethod
+  def fromkeys():
+    """fromkeys(arg1, arg2) -> object
+    MapRangesInt32.fromkeys(S,v) -> New MapRangesInt32 with keys from S and values equal to v.
+    """
+    pass
+  def update():
+    """update(arg2) -> None
+    D.update(E) -> None.  Update D from E: for k in E: D[k] = E[k]
+    """
+    pass
+  def iteritems():
+    """iteritems() -> object
+    D.iteritems() -> an iterator over the (key, value) items of D
+    """
+    pass
+  def iterkeys():
+    """iterkeys() -> object
+    D.iterkeys() -> an iterator over the keys of D
+    """
+    pass
+  def itervalues():
+    """itervalues() -> object
+    D.itervalues() -> an iterator over the values of D
+    """
     pass
 
 class RebundlerPrimaryMap:
@@ -1690,6 +1882,12 @@ class Pixelizor2_Flat:
     pass
 
 # No handler for "__version__"
+
+# No handler for "config"
+
+# No handler for "instance_config"
+
+# No handler for "soframe"
 
 # No handler for "hk"
 
