2f3043dd35fca403b807001f58c2eb1a55152c02
Brian Koopman <brian.koopman@yale.edu>
2020-04-24 13:35:30 -0400

    Remove so3g-jupyter docker build files
    This deprecates the so3g-jupyter image, it should be replaced with the ocs-util
    image [1].
    
    [1] - https://hub.docker.com/r/simonsobs/ocs-util

--- docker/so3g-jupyter/jupyter_config/set_jupyter_pw.py
+++
@@ -1,8 +0,0 @@
-from notebook.auth import passwd
-import os
-
-pw_hash = passwd()
-config_dir = os.path.join(os.environ["OCS_CONFIG_DIR"], "jupyter")
-pw_file = os.path.join(config_dir, '.jupyter_pw_hash')
-with open(pw_file, 'w+') as f:
-    f.write(pw_hash)
