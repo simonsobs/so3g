721e5f3a8b579879eda2adeee399388c690d681c
Matthew Hasselfield <mhasself@users.noreply.github.com>
2020-05-12 14:46:46 -0400

    hk: HKSession puts hkagg_version into all frames

--- python/hk/session.py
+++ python/hk/session.py
@@ -6,7 +6,7 @@ import binascii
 
 
 class HKSessionHelper:
-    def __init__(self, session_id=None, start_time=None,
+    def __init__(self, session_id=None, start_time=None, hkagg_version=None,
                  description='No description provided.'):
         """Helper class to produce G3Frame templates for creating streams of
         generic HK data.
@@ -16,8 +16,12 @@ class HKSessionHelper:
             not provided (recommended) then it will be generated based
             on the PID, the start_time, and the description string.
           start_time (float): a timestamp to use for the HK session.
+          hkagg_version (int): schema version code, which will be
+            written into every frame.  Defaults to zero, for backwards
+            compatibility.  Make sure this is set correctly.
           description (str): a description of the agent generating the
             stream.
+
         """
         if start_time is None:
             start_time = time.time()
@@ -28,6 +32,9 @@ class HKSessionHelper:
         if session_id is None:
             session_id = self._generate_session_id(start_time, description)
         self.session_id = session_id
+        if hkagg_version is None:
+            hkagg_version = 0
+        self.hkagg_version = hkagg_version
 
     @staticmethod
     def _generate_session_id(timestamp=None, description=''):
@@ -68,6 +75,7 @@ class HKSessionHelper:
         f = core.G3Frame()
         f.type = core.G3FrameType.Housekeeping
         f['hkagg_type'] = so3g.HKFrameType.session
+        f['hkagg_version'] = self.hkagg_version
         f['session_id'] = self.session_id
         f['start_time'] = self.start_time
         f['description'] = self.description
@@ -84,6 +92,7 @@ class HKSessionHelper:
         f = core.G3Frame()
         f.type = core.G3FrameType.Housekeeping
         f['hkagg_type'] = so3g.HKFrameType.status
+        f['hkagg_version'] = self.hkagg_version
         f['session_id'] = self.session_id
         f['timestamp'] = timestamp
         provs = core.G3VectorFrameObject()
@@ -107,6 +116,7 @@ class HKSessionHelper:
             timestamp = time.time()
         f = core.G3Frame()
         f.type = core.G3FrameType.Housekeeping
+        f['hkagg_version'] = self.hkagg_version
         f['hkagg_type'] = so3g.HKFrameType.data
         f['session_id'] = self.session_id
         f['prov_id'] = prov_id
