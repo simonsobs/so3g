f4117f7acd07cc7183e450c0969bc9e8e785c1d5
Brian Koopman <bjk98@cornell.edu>
2020-07-22 19:13:34 -0400

    Add HK v2 to getdata tests

--- test/test_hk_getdata.py
+++ test/test_hk_getdata.py
@@ -23,11 +23,12 @@ class Seeder(list):
     def __call__(self, *args, **kw):
         return self.Process(*args, **kw)
 
-def write_example_file(filename='hk_out.g3', hkagg_version=1):
+def write_example_file(filename='hk_out.g3', hkagg_version=2):
     """Generate some example HK data and write to file.
 
     Args:
         filename (str): filename to write data to
+        hkagg_version (int): which HK version to write to file
 
     """
     test_file = filename
@@ -37,8 +38,8 @@ def write_example_file(filename='hk_out.g3', hkagg_version=1):
     seeder = Seeder()
     w = core.G3Pipeline()
     w.Add(seeder)
-    assert(hkagg_version in [0,1])
-    if hkagg_version == 1:
+    assert(hkagg_version in [0, 1, 2])
+    if hkagg_version in [1, 2]:
         w.Add(HKTranslator)
     w.Add(core.G3Writer(test_file))
 
@@ -93,9 +94,10 @@ class TestGetData(unittest.TestCase):
     """TestCase for testing hk.getdata.py."""
     def setUp(self):
         """Generate some test HK data."""
-        self._files = ['test_0.g3', 'test_1.g3']
+        self._files = ['test_0.g3', 'test_1.g3', 'test_2.g3']
         write_example_file(self._files[0], 0)
         write_example_file(self._files[1], 1)
+        write_example_file(self._files[2], 2)
 
     def tearDown(self):
         """Remove the temporary file we made."""
