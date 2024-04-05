6e3f51113871d21ec41f666d4f1c3496e618b285
Matthew Hasselfield <mhasself@users.noreply.github.com>
2024-04-05 14:29:12 +0000

    Copy spt3g __version__, __file__ into so3g.spt3g
    This should work for either internal or external spt3g

--- python/spt3g.py
+++ python/spt3g.py
@@ -10,10 +10,10 @@ try:
     sys.modules["spt3g"] = sys.modules["so3g.spt3g_internal"]
     sys.modules["spt3g"].__name__ = "spt3g"
     del sys.modules["so3g.spt3g_internal"]
-    from spt3g import core
+    from spt3g import core, __version__, __file__
 except:
     # Not bundled
     try:
-        from spt3g import core
+        from spt3g import core, __version__, __file__
     except:
         raise ImportError("Cannot import either the internal or external spt3g!")
