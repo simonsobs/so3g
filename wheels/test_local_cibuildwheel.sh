916f14d20ae6b3f10299474502c2d154ecc1a183
Ted Kisner <tskisner.public@gmail.com>
2024-08-25 12:09:04 -0700

    Remove debugging symbols when building wheels, since it causes a 10x increase in library size.

--- wheels/test_local_cibuildwheel.sh
+++ wheels/test_local_cibuildwheel.sh
@@ -8,7 +8,7 @@ export CIBW_DEBUG_KEEP_CONTAINER=TRUE
 export CIBW_BUILD="cp311-manylinux_x86_64"
 export CIBW_MANYLINUX_X86_64_IMAGE="manylinux2014"
 export CIBW_BUILD_VERBOSITY=3
-export CIBW_ENVIRONMENT_LINUX="CC=gcc CXX=g++ CFLAGS='-O3 -g -fPIC' CXXFLAGS='-O3 -g -fPIC -std=c++14'"
+export CIBW_ENVIRONMENT_LINUX="CC=gcc CXX=g++ CFLAGS='-O3 -fPIC' CXXFLAGS='-O3 -fPIC -std=c++14'"
 export CIBW_BEFORE_BUILD_LINUX="./wheels/install_deps_linux.sh"
 export CIBW_REPAIR_WHEEL_COMMAND_LINUX="./wheels/repair_wheel_linux.sh {dest_dir} {wheel}"
 export CIBW_BEFORE_TEST="export OMP_NUM_THREADS=2"
