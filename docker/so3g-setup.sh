be9212fcff03fc785a1e2da88aa3a33d0480ac12
Theodore Kisner <tskisner.public@gmail.com>
2025-01-07 07:14:09 -0800

    Another attempt at cmake builds in docker

--- docker/so3g-setup.sh
+++ docker/so3g-setup.sh
@@ -1,12 +1,14 @@
 #!/usr/bin/env bash
 
+np_incl=$(python3 -c 'import numpy as np; print(np.get_include())')
+
 mkdir -p build
 cd build
-export PKG_CONFIG_PATH=$(numpy-config --pkgconfigdir)
 cmake \
     -DCMAKE_VERBOSE_MAKEFILE=ON \
     -DCMAKE_BUILD_TYPE=Release \
     -DPython_EXECUTABLE=$(which python3) \
+    -DPython_NumPy_INCLUDE_DIRS="${np_incl}" \
     ..
 make -j 2
 make install
