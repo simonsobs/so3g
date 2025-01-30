90328a5aea8b0f5f7dc72c0a3f0cc91d7643d66d
Matthew Hasselfield <mhasself@users.noreply.github.com>
2025-01-30 00:56:05 -0500

    Remove: hk "Reframer" class; other references to hkagg_version 0

--- python/hk/__init__.py
+++ python/hk/__init__.py
@@ -2,7 +2,6 @@
 Support for the SO Housekeeping format.
 """
 from .getdata import HKArchive, HKArchiveScanner, load_range
-from .reframer import HKReframer
 from .scanner import HKScanner
 from .session import HKSessionHelper
 from .translator import HKTranslator
