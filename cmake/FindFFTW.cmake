ccddbe781d6411574c5ceb35902e0e77e4560eb1
Michael McCrackan <mmccrack@x1402c0s6b0n1h0.chn.perlmutter.nersc.gov>
2025-02-27 22:18:56 -0800

    clean up FindFFTW

--- cmake/FindFFTW.cmake
+++ cmake/FindFFTW.cmake
@@ -1,48 +1,39 @@
-# - Find FFTW
-# Find the native FFTW includes and library
-#
+# Find FFTW
 # FFTW_INCLUDES - where to find fftw3.h
-# FFTW_LIBRARIES - List of libraries when using FFTW.
-# FFTW_FOUND - True if FFTW found.
+# FFTW_LIBRARIES - List of libraries when using FFTW
+# FFTW_FOUND - True if FFTW is found
+
 if (FFTW_INCLUDES)
-# Already in cache, be silent
-set (FFTW_FIND_QUIETLY TRUE)
-endif (FFTW_INCLUDES)
+    set (FFTW_FIND_QUIETLY TRUE)  # Already in cache, be silent
+endif ()
+
 find_path (FFTW_INCLUDES fftw3.h HINTS ENV FFTW_INC)
 find_library (FFTW_LIBRARIES NAMES fftw3 HINTS ENV FFTW_DIR)
-
-#handles finding omp implementations
-find_library (FFTW_OMP_LIBRARY NAMES fftw3_omp HINTS ENV FFTW_DIR)
+find_library (FFTW_OMP_LIBRARY NAMES fftw3_omp HINTS ENV FFTW_DIR)  # Find OMP implementation
 
 include (FindPackageHandleStandardArgs)
 set(FPHSA_NAME_MISMATCHED TRUE)
 find_package_handle_standard_args(FFTW_OMP DEFAULT_MSG FFTW_OMP_LIBRARY)
 mark_as_advanced(FFTW_OMP_LIBRARY)
 
-# handle the QUIETLY and REQUIRED arguments and set FFTW_FOUND to TRUE if
-# all listed variables are TRUE
-find_package_handle_standard_args (FFTW DEFAULT_MSG FFTW_LIBRARIES FFTW_INCLUDES)
-mark_as_advanced (FFTW_LIBRARIES FFTW_INCLUDES)
+find_package_handle_standard_args(FFTW DEFAULT_MSG FFTW_LIBRARIES FFTW_INCLUDES)
+mark_as_advanced(FFTW_LIBRARIES FFTW_INCLUDES)
 
-# FFTWF_LIBRARIES - List of libraries when using FFTWF.
-# FFTWF_FOUND - True if FFTWF is found.
-# if (FFTW_INCLUDES)
-  # Already in cache, be silent
-#  set (FFTWF_FIND_QUIETLY TRUE)
-# endif (FFTW_INCLUDES)
+# FFTWF_LIBRARIES - List of libraries when using FFTWF
+# FFTWF_FOUND - True if FFTWF is found
+
+if (FFTW_INCLUDES)
+    set (FFTWF_FIND_QUIETLY TRUE)  # Already in cache, be silent
+endif ()
 
 find_path (FFTW_INCLUDES fftw3f.h HINTS ENV FFTW_INC)
 find_library (FFTWF_LIBRARIES NAMES fftw3f HINTS ENV FFTW_DIR)
-
-# Handles finding omp implementations
-find_library (FFTWF_OMP_LIBRARY NAMES fftw3f_omp HINTS ENV FFTW_DIR)
+find_library (FFTWF_OMP_LIBRARY NAMES fftw3f_omp HINTS ENV FFTW_DIR)  # Find OMP implementation
 
 include (FindPackageHandleStandardArgs)
 set(FPHSA_NAME_MISMATCHED TRUE)
 find_package_handle_standard_args(FFTWF_OMP DEFAULT_MSG FFTWF_OMP_LIBRARY)
 mark_as_advanced(FFTWF_OMP_LIBRARY)
 
-# Handle the QUIETLY and REQUIRED arguments and set FFTWF_FOUND to TRUE if
-# all listed variables are TRUE
-find_package_handle_standard_args (FFTWF DEFAULT_MSG FFTWF_LIBRARIES FFTW_INCLUDES)
-mark_as_advanced (FFTWF_LIBRARIES FFTW_INCLUDES)
+find_package_handle_standard_args(FFTWF DEFAULT_MSG FFTWF_LIBRARIES FFTW_INCLUDES)
+mark_as_advanced(FFTWF_LIBRARIES FFTW_INCLUDES)
